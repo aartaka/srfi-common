8414408099af0cd41fcb880f6ca612e1f87a3dc7
Artyom Bologov <git@aartaka.me>
2026-09-15 02:00:44 +0400

    Make color-scheme toggle slightly friendlier to screen readers

--- dark-light.js
+++ dark-light.js
@@ -1,11 +1,11 @@
 !function(s, k, mq, r) {
-  var p = s.getItem(k), v = [null, 'light', 'dark'],
-      S = '<svg viewBox="0 0 20 20" width="1em" height="1em">',
-      C = '<circle cx="10" cy="10" r="9" ',
-      c = [S + C + 'fill="none" stroke="currentColor" stroke-width="2"/><path d="M10 1A9 9 0 0 0 10 19Z" fill="currentColor"/></svg>',
-           S + C + 'fill="none" stroke="currentColor" stroke-width="2"/></svg>',
-           S + C + 'fill="black" stroke="white" stroke-width="2"/></svg>'];
-  function a(t) { r.dataset.theme = t || (mq.matches ? 'dark' : 'light') }
+    var p = s.getItem(k), v = [null, 'light', 'dark'],
+        S = '<span class=sr-only>Toggle Dark / Light theme</span><svg viewBox="0 0 20 20" width="1em" height="1em">',
+        C = '<circle cx="10" cy="10" r="9" ',
+        c = [S + C + 'fill="none" stroke="currentColor" stroke-width="2"/><path d="M10 1A9 9 0 0 0 10 19Z" fill="currentColor"/></svg>',
+             S + C + 'fill="none" stroke="currentColor" stroke-width="2"/></svg>',
+             S + C + 'fill="black" stroke="white" stroke-width="2"/></svg>'];
+    function a(t) { r.dataset.theme = t || (mq.matches ? 'dark' : 'light') }
   a(p);
   mq.onchange = function() { s.getItem(k) || a() };
   document.addEventListener('DOMContentLoaded', function() {
